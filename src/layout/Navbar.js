11c8977a31f1ae0d80eeda4045c3b585e1e09c41
xjmx315 <xjmx315@gmail.com>
2025-03-27 00:26:57 +0900

    로고 호버링 효과 추가

--- src/layout/Navbar.js
+++ src/layout/Navbar.js
@@ -10,9 +10,9 @@ function Navbar() {
     <nav>
       <h1 id='logo'>Highball Maniac</h1>
       <ul>
-        <li><NavLink exact to="/" activeClassName="active">Home</NavLink></li>
-        <li><NavLink to="/about" activeClassName="active">About</NavLink></li>
-        <li><NavLink to="/contact" activeClassName="active">Contact</NavLink></li>
+        <li><NavLink exact to="/" activeClassName="active">조합</NavLink></li>
+        <li><NavLink to="/about" activeClassName="active">발견</NavLink></li>
+        <li><NavLink to="/contact" activeClassName="active">About</NavLink></li>
       </ul>
     </nav>
   );
