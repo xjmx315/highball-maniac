a29db37904c12c74a4ad8889176ab4c95206efed
xjmx315 <xjmx315@gmail.com>
2025-03-28 13:45:33 +0900

    추가 버튼 추가

--- src/components/Item.js
+++ src/components/Item.js
@@ -3,9 +3,9 @@
 import React from "react";
 import './Item.css'
 
-function Item({ imageUrl, description }) {
+function Item({ imageUrl, description, _onClick }) {
     return (
-      <div className="item-container">
+      <div className="item-container" onClick={_onClick}>
         <img src={imageUrl} alt={description} className="item-image" />
         <p className="item-description">{description}</p>
       </div>
--- src/pages/Discover.js
+++ src/pages/Discover.js
@@ -1,10 +1,25 @@
 //Discover.js
 
-import React from "react";
+import React, { useState } from "react";
 import Item from "../components/Item";
 import './Discover.css';
 
 function Discover(){
+    const [items, setItems] = useState([]);
+
+    const testItem = () => {
+        return (
+            <Item
+                imageUrl="https://m.hellobaking.com/web/product/big/mamsbaking_1182.jpg"
+                description="바카디 모히또"
+            />
+        );
+    };
+
+    const addItem = () => {
+        setItems([...items, testItem()]);
+    };
+
     return (
         <div style={{ display: 'flex', justifyContent: 'center', padding: '20px' }}>
             <Item
@@ -14,6 +29,12 @@ function Discover(){
             <Item
                 imageUrl="https://spng.pngfind.com/pngs/s/623-6233285_bacardi-oakheart-spiced-rum-1-litre-png-download.png"
                 description="바카디 럼"
+            />
+            {items}
+            <Item 
+                imageUrl="https://cdn-icons-png.flaticon.com/512/10233/10233645.png"
+                description="추가"
+                _onClick={addItem}
             />
         </div>
     );
