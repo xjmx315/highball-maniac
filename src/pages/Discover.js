16e84d1edefd3ddeb18d467d4a99d74cec2c2dc1
xjmx315 <xjmx315@gmail.com>
2025-03-31 00:34:22 +0900

    발견 버튼 추가

--- src/pages/Discover.js
+++ src/pages/Discover.js
@@ -21,21 +21,24 @@ function Discover(){
     };
 
     return (
-        <div style={{ display: 'flex', justifyContent: 'center', padding: '20px' }}>
-            <Item
-                imageUrl="https://m.hellobaking.com/web/product/big/mamsbaking_1182.jpg"
-                description="바카디 모히또"
-            />
-            <Item
-                imageUrl="https://spng.pngfind.com/pngs/s/623-6233285_bacardi-oakheart-spiced-rum-1-litre-png-download.png"
-                description="바카디 럼"
-            />
-            {items}
-            <Item 
-                imageUrl="https://cdn-icons-png.flaticon.com/512/10233/10233645.png"
-                description="추가"
-                _onClick={addItem}
-            />
+        <div>
+            <div style={{ display: 'flex', justifyContent: 'center', padding: '20px' }}>
+                <Item
+                    imageUrl="https://m.hellobaking.com/web/product/big/mamsbaking_1182.jpg"
+                    description="바카디 모히또"
+                />
+                <Item
+                    imageUrl="https://spng.pngfind.com/pngs/s/623-6233285_bacardi-oakheart-spiced-rum-1-litre-png-download.png"
+                    description="바카디 럼"
+                />
+                {items}
+                <Item 
+                    imageUrl="https://cdn-icons-png.flaticon.com/512/10233/10233645.png"
+                    description="추가"
+                    _onClick={addItem}
+                />
+            </div>
+            <button id="discoverButton">발견!</button>
         </div>
     );
 }
