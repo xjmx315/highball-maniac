0f873293b843c4eebc72d612253a31afc0eab797
xjmx315 <xjmx315@gmail.com>
2025-08-01 20:18:48 +0900

    RecipeContainer 적용

---
+++ src/components/RecipeContainer.js
@@ -0,0 +1,54 @@
+//RecipeContainer.js
+
+import React, {useEffect, useState} from 'react';
+import apiClient from '../common/apiClient';
+import Card from './Card';
+import { useNavigate } from 'react-router-dom';
+
+const makeCard = (recipeData, navigate) => {
+    //필수 항목 검사
+
+    const { name, image, id } = recipeData;
+    if (!name || !image || !id) {
+        return (<h1>ERR!</h1>);
+    }
+
+    const redirectFun = () => {
+        navigate(`/recipe/?name=${name}`);
+    }
+
+    return (
+        <Card imageUrl={image} description={name} _onClick={redirectFun}/>
+    );
+};
+
+const RecipeContainer = ({ headLine, dis, apiURL }) => {
+    const [recipes, setRecipes] = useState([]);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const data = apiClient.get(apiURL);
+        data.then((res) => {
+            if (!res.ok) {
+                console.log('recipeContainer API 호출 실패:', res.message);
+            }
+            else {
+                setRecipes(res.data.recipes.map((value) => {
+                    makeCard(value, navigate);
+                }));
+            }
+        });
+    }, [recipes]);
+
+    return (
+        <div className="recipe-container">
+            <h1>{headLine}</h1>
+            <p>{dis}</p>
+            <div className="recipe-card-container">
+                {recipes}
+            </div>
+        </div>
+    );
+}
+
+export default RecipeContainer;
--- src/pages/Recipes.js
+++ src/pages/Recipes.js
@@ -2,18 +2,14 @@
 
 import React from "react";
 import './Recipes.css';
+import RecipeContainer from "../components/RecipeContainer";
 
 function Recipes(){
     return (
         <div id="recipes">
-            <h1>인기 레시피</h1>
-            <p>Highball Maniac의 최고 인기 레시피를 만나보세요</p>
-            
-            <h1>새로운 레시피</h1>
-            <p>가장 빠르게 새롭게 추가된 레시피를 만나보세요</p>
-
-            <h1>랜덤 레시피</h1>
-            <p>새로운 레시피를 발견하세요</p>
+            <RecipeContainer headLine={'인기 레시피'} dis={'Highball Maniac의 최고 인기 레시피를 만나보세요'}/>
+            <RecipeContainer headLine={'새로운 레시피'} dis={'가장 빠르게 새롭게 추가된 레시피를 만나보세요'}/>
+            <RecipeContainer headLine={'랜덤 레시피'} dis={'새로운 레시피를 발견하세요'}/>
         </div>
     );
 }
