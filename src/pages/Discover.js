aa91434eb7566de6991aec4775ea4bcefacff3eb
xjmx315 <xjmx315@gmail.com>
2025-08-26 17:46:30 +0900

    레시피 검색 기능 api 연동

--- src/pages/Discover.js
+++ src/pages/Discover.js
@@ -2,16 +2,31 @@
 
 import Items from "../components/Items";
 import useItems from "../components/useItems";
+import { createPopup } from "../components/Popup";
+import RecipeContainer from "../components/RecipeContainer";
+import { useState } from "react";
 
 import './Discover.css';
 
 function Discover(){
     const {items, setItems} = useItems();
+    const [searchUrl, setSearchUrl] = useState('');
+
+    const getItemData = () => {
+        return items.map(value => value.itemId).join(',');
+    };
+
+    const searchRecipes = async () => {
+        const searchString = getItemData();
+        console.log(searchString);
+        setSearchUrl(`/recipe/including/?items=${searchString}`);
+    };
     
     return (
         <div>
             <Items selectedItems={items} setSelectedItems={setItems}/>
-            <button id="discoverButton">발견!</button>
+            <button className="enter-button" id="discoverButton" onClick={searchRecipes}>발견!</button>
+            <RecipeContainer headLine={"검색 결과"} dis={""} apiURL={searchUrl}/>
         </div>
     );
 }
