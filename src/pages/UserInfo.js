e24080f08317cc95044cd2b680b6417494a15a84
xjmx315 <xjmx315@gmail.com>
2025-08-30 17:09:55 +0900

    user 페이지에 업로드한 레시피 추가

--- src/pages/UserInfo.js
+++ src/pages/UserInfo.js
@@ -4,6 +4,7 @@ import apiClient from "../common/apiClient";
 import { createPopup } from "../components/Popup";
 import { useParams, useNavigate } from "react-router-dom";
 import {useUser} from "../common/UserContext";
+import RecipeContainer from "../components/RecipeContainer";
 
 const getUserInfo = async (userName) => {
     console.log("get userinfo with:", userName);
@@ -19,6 +20,7 @@ const UserInfo = ({isSelfPage}) => {
     const {userName} = useParams();
     const [userNameState, setUserName] = useState("Loding...");
     const [userCreatedAt, setUserCreatedAt] = useState("");
+    const [userId, setUserId] = useState("");
 
     const navigate = useNavigate();
     const {isLoggedIn, user, checkToken} = useUser();
@@ -29,10 +31,12 @@ const UserInfo = ({isSelfPage}) => {
         if (isSelfPage) {
             if (isLoggedIn){
                 navigate(`/user_info/${user}`);
+                return ;
             }
             else{
                 createPopup('로그인 후 이용해 주세요.');
                 navigate('/login');
+                return ;
             }
         }
 
@@ -42,18 +46,21 @@ const UserInfo = ({isSelfPage}) => {
             if (res){
                 setUserName(res.userName);
                 setUserCreatedAt(res.created_at);
+                setUserId(res.userId);
             }
             else {
                 setUserName("User not found");
                 setUserCreatedAt("");
+                setUserId("");
             }
         });
-    }, [userNameState, userCreatedAt, isSelfPage]);
+    }, [isSelfPage]);
 
     return (
         <div>
             <h1>{userNameState}</h1>
             <p>{userCreatedAt}</p>
+            <RecipeContainer headLine={"업로드한 레시피"} apiURL={userId && `/recipe/writtenby/${userId}`}/>
         </div>
     );
 };
